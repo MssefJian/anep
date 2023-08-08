85c2d8f26ae4067a8c76d1d1118f6aaca9dcb47d
mssefjian <youssef.programmeur@gmail.com>
2023-08-08 19:12:02 +0100

    create anep theme

---
+++ web/themes/anep/js/anep.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * anep behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.anep = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
