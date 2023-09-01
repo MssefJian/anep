f05d14e7be2d28499568f4ac6dcec139ed61b554
mssefjian <youssef.programmeur@gmail.com>
2023-09-01 21:22:09 +0100

    Presentation page

--- web/modules/custom/md_manager/src/Controller/CustomPagesController.php
+++ web/modules/custom/md_manager/src/Controller/CustomPagesController.php
@@ -5,10 +5,11 @@ namespace Drupal\md_manager\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\node\Entity\Node;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 
-class OrganigramController extends ControllerBase
+class CustomPagesController extends ControllerBase
 {
 
   protected $entityTypeManager;
@@ -16,24 +17,28 @@ class OrganigramController extends ControllerBase
   protected $languageManager;
 
 
-  public function __construct(EntityTypeManagerInterface $entityTypeManager, LanguageManagerInterface $languageManager) {
+  public function __construct(EntityTypeManagerInterface $entityTypeManager, LanguageManagerInterface $languageManager)
+  {
     $this->entityTypeManager = $entityTypeManager;
     $this->languageManager = $languageManager;
   }
 
-  public static function create(ContainerInterface $container) {
+  public static function create(ContainerInterface $container)
+  {
     return new static(
       $container->get('entity_type.manager'),
       $container->get('language_manager')
     );
   }
+
   /**
-   * Display the custom text page.
+   * Display the Organigramme page.
    *
    * @return array
    *   A render array containing the static text.
    */
-  public function build() {
+  public function organigramme()
+  {
 
     $language = $this->languageManager->getCurrentLanguage();
     $currentLang = $language->getId();
@@ -49,7 +54,7 @@ class OrganigramController extends ControllerBase
     if (!empty($block_ids)) {
       $block_id = reset($block_ids);
       $block = $this->entityTypeManager->getStorage('block_content')->load($block_id);
-      if ($block->hasTranslation($language->getId())) {
+      if ($block->hasTranslation($currentLang)) {
         $translatedBlock = $block->getTranslation($currentLang);
         $content = $translatedBlock->get('body')->value;
       } else {
@@ -62,6 +67,43 @@ class OrganigramController extends ControllerBase
       '#theme' => 'organigramme',
       '#currentLang' => $currentLang,
       '#content' => $content ?? NULL,
+    ];
+  }
+
+  /**
+   * Display Presentation page
+   * @return array
+   *    A render array containing the static text.
+   */
+  public function presentation()
+  {
+
+    $langcode = $this->languageManager->getCurrentLanguage()->getId();
+    $query = \Drupal::entityQuery('node')
+      ->condition('type', 'timeline')
+      ->condition('langcode', $langcode, '=')
+      ->condition('status', 1)
+      ->accessCheck(FALSE)
+      ->execute();
+
+
+// Load the node entities using the node IDs.
+    $nodes = Node::loadMultiple($query);
+    $timeLines = [];
+    foreach ($nodes as $node) {
+
+      $translation = $node->hasTranslation($langcode);
+      $tmp['title'] = $translation ? $node->getTranslation($langcode)->get('title')->value : $node->getTitle();
+      $tmp['year'] = $translation ? $node->getTranslation($langcode)->get('field_year')->value : $node->get('field_year')->value;
+      $timeLines[] = $tmp;
+    }
+
+    return [
+      '#theme' => 'timeline',
+      '#timeLines' => $timeLines,
+      '#attached' => [
+        'library' => ['md_manager/timeline']
+      ]
     ];
   }
 }
